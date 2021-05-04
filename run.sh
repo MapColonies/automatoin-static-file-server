c6fa70f94050d58ef62c544255c549e01e26f578
ronenk1 <ronenk1@rnd-hub.com>
2021-05-04 15:05:45 +0300

    chore(automation): deploy

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ IMAGE="serve:latest"
 # Finally run
 docker stop ${NAME} > /dev/null 2>&1
 docker rm ${NAME} > /dev/null 2>&1
-docker run --net=host --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
+docker run --net=host --name ${NAME} -i -v ${PWD}:/opt/relative_folder ${IMAGE}
